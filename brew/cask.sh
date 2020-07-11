7b60f83816fdba1968a3e26265f86e56686fd8e9
Ikigai-pt <32212975+Ikigai-pt@users.noreply.github.com>
2020-07-11 15:16:20 -0600

    Update cask.sh

--- brew/cask.sh
+++ brew/cask.sh
@@ -6,3 +6,9 @@ brew cask install ngrok
 brew cask install visual-studio-code
 brew cask install font-firacode-nerd-font
 brew cask install sqlworkbenchj
+brew cask install clipy
+brew cask install dbeaver-community
+brew cask install apptivate
+brew cask install loom
+brew cask install postman
+
